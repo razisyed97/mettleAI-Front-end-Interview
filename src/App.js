2caacaab74028fe8798203f6661c866d1d1fa87a
Daniel Yu <danielyu2004@icloud.com>
2020-04-23 00:51:06 -0400

    d

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+import Referral from './components/Referral'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+import BrowserRouter from "react-router-dom/BrowserRouter";
+import Route from 'react-router-dom/Route'
+import Switch from 'react-router-dom/Switch'
+
+import {MuiThemeProvider} from '@material-ui/core/styles'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+
+const THEME = createMuiTheme({
+  typography: {
+   "fontFamily": "'Montserrat', sans-serif;",
+  },
+  palette : {
+    primary : {
+      main: '#3A719B',
+    },
+    secondary : {
+      main: '#3A719B',
+    }
+  }
+});
+
+class App extends Component {
+  render(){
+    return (
+      <div className="app-div">
+        <MuiThemeProvider theme={THEME}>
+
+          <BrowserRouter>
+            <Navbar/>
+            <Switch>     
+              <Route to="/" component={Referral}/>
+            </Switch>
+            <Footer/>
+          </BrowserRouter>
+        </MuiThemeProvider>
+
+      </div>
+ 
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render(){
+        return (
+            <div style={{width: "100%", height: "300px"}}>
+
+            </div>
+        )
+    }
+}
+
+export default Footer
