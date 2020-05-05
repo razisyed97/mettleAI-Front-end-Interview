a92ed51722a8c0141849ec258b50db584c851afb
Fatima Altaf <fatima.altaf@gmail.com>
2020-05-05 14:01:33 -0400

    header js and css file add

---
+++ src/components/UI/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h3>Patient Referral Form</h3>
+      <p>Hayes Valley Health San Francisco</p>
+    </div>
+  );
+}
