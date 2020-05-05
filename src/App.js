79c312a07a33e03fe1edeb0a79a7736dfb526495
Fatima Altaf <fatima.altaf@gmail.com>
2020-05-05 13:56:14 -0400

    add form component to app and render form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/UI/Header";
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header />
+      <main className="App-header">
+        <Form />
+      </main>
     </div>
   );
 }
